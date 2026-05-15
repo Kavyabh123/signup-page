762db09ec7f6d4e4f4c3b81ab566f03099503d42
Kavya B <hachadadkavyab@gmail.com>
2026-05-15 12:59:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Signup from "./Signup"; // make sure Signup.js is in src/
+
+function App() {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
+
